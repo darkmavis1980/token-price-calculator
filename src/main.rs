4f6352adec66a77cde11685f0cafb3ac4e01d086
Alessio Michelini <amichelini@hubspot.com>
2024-02-27 12:11:12 +0000

    fix: code cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+struct CostResponse {
+    num_requests: f32,
+    cost: f32,
+}
+
+impl CostResponse {
+    fn calculate_cost(tokens: f32, requests: f32, pricing: f32) -> Self {
+        let num_requests = tokens * requests;
+        let cost_per_thousand_requests = pricing / 1000.0;
+        let cost = num_requests * cost_per_thousand_requests;
+
+        CostResponse { num_requests, cost }
+    }
+}
+
 fn main() {
     let mut input = String::new();
     let mut output = String::new();
@@ -16,12 +31,9 @@ fn main() {
     std::io::stdin().read_line(&mut num_request).unwrap();
     let requests_num: f32 = num_request.trim().parse::<f32>().unwrap();
 
-    let input_requests: f32 =  input_tokens * requests_num;
-    let input_cost: f32 = (input_requests / 1000.0) * _price_input_1k;
+    let input_cost_response = CostResponse::calculate_cost(input_tokens, requests_num, _price_input_1k);
+    let output_cost_response = CostResponse::calculate_cost(output_tokens, requests_num, _price_output_1k);
 
-    let output_requests: f32 = output_tokens * requests_num;
-    let output_cost: f32 = (output_requests / 1000.0) * _price_output_1k;
-
-    println!("The prompt tokens will cost: {}USD, for {} tokens generated", input_cost, input_requests);
-    println!("The completition tokens will cost: {}USD, for {} tokens generated", output_cost, output_requests);
+    println!("The prompt tokens will cost: {}USD, for {} tokens generated", input_cost_response.cost, input_cost_response.num_requests);
+    println!("The completition tokens will cost: {}USD, for {} tokens generated", output_cost_response.cost, output_cost_response.num_requests);
 }
