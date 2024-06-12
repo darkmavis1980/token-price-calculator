0ac469be7c301490cb428e2d1442ce38746ca940
Alessio Michelini <amichelini@hubspot.com>
2024-06-12 10:10:20 +0100

    feat: removed gpt-3.5-turbo-0125 model and docs cleanup

--- src/prices.rs
+++ src/prices.rs
@@ -1,27 +1,25 @@
 pub fn get_model_prices(model: &str) -> (f32, f32) {
-  match model {
-      "gpt-3.5-turbo" => (0.0005, 0.0015),
-      "gpt-3.5-turbo-0125" => (0.0005, 0.0015),
-      "gpt-3.5-turbo-instruct" => (0.0015, 0.002),
-      "gpt-4" => (0.03, 0.06),
-      "gpt-4-32k" => (0.06, 0.12),
-      "gpt-4-turbo" => (0.01, 0.03),
-      "gpt-4o" => (0.005, 0.015),
-      "gemini" => (0.00035, 0.00105),
-      "gemini_128k" => (0.0007, 0.0021),
-      _ => (0.0005, 0.0015)
-  }
+    match model {
+        "gpt-3.5-turbo" => (0.0005, 0.0015),
+        "gpt-3.5-turbo-instruct" => (0.0015, 0.002),
+        "gpt-4" => (0.03, 0.06),
+        "gpt-4-32k" => (0.06, 0.12),
+        "gpt-4-turbo" => (0.01, 0.03),
+        "gpt-4o" => (0.005, 0.015),
+        "gemini" => (0.00035, 0.00105),
+        "gemini_128k" => (0.0007, 0.0021),
+        _ => (0.005, 0.015)
+    }
 }
 
-pub const MODELS: [&str; 8] = [
-  "gpt-3.5-turbo",
-  "gpt-3.5-turbo-0125",
-  "gpt-3.5-turbo-instruct",
-  "gpt-4",
-  "gpt-4-32k",
-  "gpt-4-turbo",
-  "gpt-4o",
-  "gemini",
+pub const MODELS: [&str; 7] = [
+    "gemini",
+    "gpt-4o",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-instruct",
+    "gpt-4",
+    "gpt-4-32k",
+    "gpt-4-turbo",
 ];
 
 #[cfg(test)]
@@ -31,13 +29,6 @@ mod tests {
     #[test]
     fn test_get_model_prices_gpt_3_5_turbo() {
         let (input_price, output_price) = get_model_prices("gpt-3.5-turbo");
-        assert_eq!(input_price, 0.0005);
-        assert_eq!(output_price, 0.0015);
-    }
-
-    #[test]
-    fn test_get_model_prices_gpt_3_5_turbo_0125() {
-        let (input_price, output_price) = get_model_prices("gpt-3.5-turbo-0125");
         assert_eq!(input_price, 0.0005);
         assert_eq!(output_price, 0.0015);
     }
@@ -80,8 +71,8 @@ mod tests {
     #[test]
     fn test_get_model_prices_unknown() {
         let (input_price, output_price) = get_model_prices("unknown");
-        assert_eq!(input_price, 0.0005);
-        assert_eq!(output_price, 0.0015);
+        assert_eq!(input_price, 0.005);
+        assert_eq!(output_price, 0.015);
     }
 
     #[test]
