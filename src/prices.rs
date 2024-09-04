3ddf8828107acd0f5a24d47044230d8bb8e03f41
Alessio Michelini <amichelini@hubspot.com>
2024-09-04 12:14:30 +0100

    fix: re-sorted list of openai models

--- src/prices.rs
+++ src/prices.rs
@@ -54,14 +54,14 @@ pub const PROVIDERS: [&str; 3] = ["openai", "google", "perplexity"];
 pub fn get_provider_models(provider: &str) -> Vec<&str> {
     match provider {
         "openai" => vec![
-            "gpt-3.5-turbo",
-            "gpt-3.5-turbo-instruct",
-            "gpt-4",
-            "gpt-4-32k",
-            "gpt-4-turbo",
             "gpt-4o",
             "gpt-4o-2024-08-06",
             "gpt-4o-mini",
+            "gpt-4",
+            "gpt-4-32k",
+            "gpt-4-turbo",
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-instruct",
         ],
         "google" => vec![
             "gemini-1-pro",
