53efab83a26cb99827fbb74c4cf69b4fe722059a
Alessio Michelini <amichelini@hubspot.com>
2025-08-14 12:20:42 +0100

    fix: update model prices for Google, OpenAI, and Perplexity providers; adjust cost calculation to per million requests

--- src/cost/mod.rs
+++ src/cost/mod.rs
@@ -6,8 +6,8 @@ pub struct CostResponse {
 impl CostResponse {
   pub fn calculate_cost(tokens: i32, requests: i32, pricing: f32) -> Self {
       let num_tokens: i32 = tokens * requests;
-      let cost_per_thousand_requests = pricing / 1000.0;
-      let cost = num_tokens as f32 * cost_per_thousand_requests;
+      let cost_per_million_requests = pricing / 1000000.0;
+      let cost = num_tokens as f32 * cost_per_million_requests;
 
       CostResponse { num_tokens, cost }
   }
@@ -19,21 +19,21 @@ mod tests {
 
     #[test]
     fn test_calculate_cost() {
-        let cost_response = CostResponse::calculate_cost(100, 10, 0.0005);
+        let cost_response = CostResponse::calculate_cost(100, 10, 0.5);
         assert_eq!(cost_response.num_tokens, 1000);
-        assert_eq!(cost_response.cost, 0.0005);
+        assert_eq!(cost_response.cost, 0.5);
     }
 
     #[test]
     fn test_calculate_cost_high() {
-      let cost_response = CostResponse::calculate_cost(100, 10000, 0.0005);
+      let cost_response = CostResponse::calculate_cost(100, 10000, 0.5);
       assert_eq!(cost_response.num_tokens, 1000000);
       assert_eq!(cost_response.cost, 0.5);
     }
 
     #[test]
     fn test_calculate_cost_zero() {
-        let cost_response = CostResponse::calculate_cost(0, 0, 0.0005);
+        let cost_response = CostResponse::calculate_cost(0, 0, 0.5);
         assert_eq!(cost_response.num_tokens, 0);
         assert_eq!(cost_response.cost, 0.0);
     }
